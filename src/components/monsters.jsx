2b67de9572cae6f9730697f7b2d900cade22f991
Erison <erisonn.santos@gmail.com>
2021-06-24 02:01:36 -0300

    locations and drops

--- src/components/monsters.jsx
+++ src/components/monsters.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Monsters = ({monstersList}) => {
-
   return (  
 
     <div className='monsters'>
@@ -10,6 +9,14 @@ const Monsters = ({monstersList}) => {
           <h1>{monster.name.toUpperCase()}</h1>
           <img src={monster.image} alt={monster.name}/>
           <p>{monster.description}</p>
+          <ul>
+            Common Locations {monster.common_locations === null || monster.common_locations.length === 0 ? 
+            <li>Hyrule</li> : monster.common_locations.map(location => <li key={location + monster.id}>{location}</li>)}
+            <ul>
+              Drops {monster.drops == null || monster.drops.length === 0 ? 
+              <li>None</li> : monster.drops.map(drop => <li key={drop + monster.id}>{drop.charAt(0).toUpperCase() + drop.slice(1)}</li>)}
+            </ul>
+          </ul>
         </div>
       )}
     </div>
